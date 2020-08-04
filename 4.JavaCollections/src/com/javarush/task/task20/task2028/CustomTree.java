e07b30c04efacfcbf31de9aded84b7b890283510
Dima <nevar.dima@gmail.com>
2020-08-04 17:05:31 +0300

    add task2028

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,14 +1,20 @@
 package com.javarush.task.task20.task2028;
 
+import sun.management.snmp.jvmmib.EnumJvmRTBootClassPathSupport;
+
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /*
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+    Entry<String> root;
+
+    CustomTree(){
+        this.root = new Entry<>("0");
+    }
 
     @Override
     public String get(int index) {
@@ -17,7 +23,25 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+        int count = 0;
+        ArrayDeque<Entry<String>> deque = new ArrayDeque<>();
+        Entry<String> tempEntry = root;
+        deque.add(tempEntry);
+
+        do{
+            if(!deque.isEmpty()){
+                tempEntry = deque.poll();
+            }
+            if(tempEntry.leftChild != null){
+                deque.add(tempEntry.leftChild);
+                count++;
+            }
+            if(tempEntry.rightChild != null){
+                deque.add(tempEntry.rightChild);
+                count++;
+            }
+        }while (!deque.isEmpty());
+        return count;
     }
 
     @Override
@@ -48,5 +72,105 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     @Override
     public boolean addAll(int index, Collection<? extends String> c) {
         throw new UnsupportedOperationException();
+    }
+
+    public String getParent(String elementName){
+        Entry<String> tempEntry = findEntry(elementName);
+        if(tempEntry == null){
+            return null;
+        }
+        return tempEntry.parent.elementName;
+    }
+
+    public boolean add(String element) {
+        ArrayDeque<Entry<String>> deque = new ArrayDeque<>();
+        Entry<String> tempEntry = root;
+        deque.add(tempEntry);
+        do{
+            if(!deque.isEmpty()){
+                tempEntry = deque.poll();
+            }
+            if(!tempEntry.availableToAddLeftChildren && !tempEntry.availableToAddRightChildren &&
+            tempEntry.leftChild == null && tempEntry.rightChild == null){
+                tempEntry.availableToAddLeftChildren = true;
+                tempEntry.availableToAddRightChildren = true;
+            }
+            if(tempEntry.leftChild != null){
+                deque.add(tempEntry.leftChild);
+            }else if(tempEntry.availableToAddLeftChildren){
+                tempEntry.leftChild = new Entry<>(element);
+                tempEntry.leftChild.parent = tempEntry;
+                return true;
+            }
+            if(tempEntry.rightChild != null){
+                deque.add(tempEntry.rightChild);
+            }else if(tempEntry.availableToAddRightChildren){
+                tempEntry.rightChild = new Entry<>(element);
+                tempEntry.rightChild.parent = tempEntry;
+                return true;
+            }
+        }while (!deque.isEmpty());
+        return false;
+
+    }
+
+    public boolean remove(Object o){
+        String elementName;
+        try{
+            elementName = (String) o;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+        Entry<String> tempEntry = findEntry(elementName);
+        if(tempEntry.equals(tempEntry.parent.leftChild)){
+            tempEntry.parent.leftChild = null;
+            tempEntry.parent.availableToAddLeftChildren = false;
+            return true;
+        }
+        if(tempEntry.equals(tempEntry.parent.rightChild)){
+            tempEntry.parent.rightChild = null;
+            tempEntry.parent.availableToAddRightChildren = false;
+            return true;
+        }
+        return false;
+    }
+
+    public Entry<String> findEntry(String elementName){
+        ArrayDeque<Entry<String>> deque = new ArrayDeque<>();
+        Entry<String> tempEntry = root;
+        deque.add(tempEntry);
+
+        do{
+            if(!deque.isEmpty()){
+                tempEntry = deque.poll();
+            }
+            if(tempEntry.elementName.equals(elementName)){
+                return tempEntry;
+            }
+            if(tempEntry.leftChild != null){
+                deque.add(tempEntry.leftChild);
+            }
+            if(tempEntry.rightChild != null){
+                deque.add(tempEntry.rightChild);
+            }
+        }while (!deque.isEmpty());
+        return null;
+    }
+
+    static class Entry<T> implements Serializable{
+        String elementName;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName){
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren(){
+            return availableToAddLeftChildren || availableToAddRightChildren;
+        }
     }
 }
