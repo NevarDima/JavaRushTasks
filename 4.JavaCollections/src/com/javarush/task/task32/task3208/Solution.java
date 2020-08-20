7c5481c053959abe9c9964c1176cdf1cf7208e8d
Dima <nevar.dima@gmail.com>
2020-08-20 15:32:26 +0300

    add task3208

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3208/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task32.task3208;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/* 
+RMI-2
+*/
+public class Solution {
+    public static Registry registry;
+
+    // Pretend we're starting an RMI client as the CLIENT_THREAD thread
+    public static Thread CLIENT_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                for (String bindingName : registry.list()) {
+                    Animal service = (Animal) registry.lookup(bindingName);
+                    service.printName();
+                    service.speak();
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    // Pretend we're starting an RMI server as the SERVER_THREAD thread
+    public static Thread SERVER_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            //напишите тут ваш код
+            try {
+                final String UNIQUE_BINDING_NAME = "server.animal";
+
+                registry = LocateRegistry.createRegistry(2099);
+
+                final Cat server1 = new Cat("cat");
+                final Dog server2 = new Dog("dog");
+
+                Remote stub1 = UnicastRemoteObject.exportObject(server1, 0);
+                Remote stub2 = UnicastRemoteObject.exportObject(server2, 0);
+
+                registry.bind(UNIQUE_BINDING_NAME, stub1);
+                registry.bind(UNIQUE_BINDING_NAME, stub2);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (AlreadyBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+        // Starting an RMI server thread
+        SERVER_THREAD.start();
+        Thread.sleep(1000);
+        // Start the client
+        CLIENT_THREAD.start();
+    }
+}
