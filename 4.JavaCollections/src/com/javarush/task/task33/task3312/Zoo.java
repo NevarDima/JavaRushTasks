ae56dfa50937b82098eb733f0284843ad7ccae5c
Dima Nevar <nevar.dima@gmail.com>
2020-09-25 11:46:44 +0300

    add task3312

--- 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
@@ -2,18 +2,20 @@ package com.javarush.task.task33.task3312;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property="type")
-@JsonSubTypes({
-        @JsonSubTypes.Type(value=Zoo.Cat.class, name="cat"),
-        @JsonSubTypes.Type(value=Zoo.Dog.class, name="dog")
-})
+
 public class Zoo {
     public List<Animal> animals = new ArrayList<>();
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property="type")
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value=Zoo.Cat.class, name="cat"),
+            @JsonSubTypes.Type(value=Zoo.Dog.class, name="dog")
+    })
     public static class Animal {
         public Animal(String name) {
             this.name = name;
@@ -22,6 +24,7 @@ public class Zoo {
         public String name;
     }
 
+    @JsonTypeName("dog")
     public static class Dog extends Animal {
 
         public double barkVolume;
@@ -31,6 +34,7 @@ public class Zoo {
         }
     }
 
+    @JsonTypeName("cat")
     public static class Cat extends Animal {
         boolean likesCream;
         public int lives;
