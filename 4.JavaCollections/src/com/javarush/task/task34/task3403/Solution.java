587053890aca98e8e90704745e7d24dc8d6aa43a
Dima Nevar <nevar.dima@gmail.com>
2021-01-27 17:16:11 +0300

    add task3403

--- 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -6,5 +6,16 @@ package com.javarush.task.task34.task3403;
 
 public class Solution {
     public void recurse(int n) {
+        int currentValue = n;
+        int multiplier = 2;
+        while (multiplier <= currentValue) {
+            if (currentValue % multiplier == 0) {
+                System.out.print(multiplier + " ");
+                if (multiplier == currentValue){return;}
+                recurse(currentValue / multiplier);
+                break;
+            }
+            multiplier++;
+        }
     }
 }
